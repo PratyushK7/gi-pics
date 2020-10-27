ed17769a56833b13f595639e37370750a15ec73e
Pratyush Kumar <pratyushnjr9@gmail.com>
2020-10-28 00:34:48 +0530

    Updates

---
+++ precache-manifest.887f077f6b43e5ac6063e4bce0f9ab15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "709e753485ba566deca355bff5d6d37e",
+    "url": "/gi-pics/index.html"
+  },
+  {
+    "revision": "1921bcb19117ab88ebcd",
+    "url": "/gi-pics/static/css/main.bc17ee65.chunk.css"
+  },
+  {
+    "revision": "9cd49581548832788f0f",
+    "url": "/gi-pics/static/js/2.88b0c596.chunk.js"
+  },
+  {
+    "revision": "63ac404d1d3777a10c65b5bf2de6bc03",
+    "url": "/gi-pics/static/js/2.88b0c596.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1921bcb19117ab88ebcd",
+    "url": "/gi-pics/static/js/main.dabd2622.chunk.js"
+  },
+  {
+    "revision": "d1c4677b5f4680a8a9f5",
+    "url": "/gi-pics/static/js/runtime-main.0c436aa8.js"
+  },
+  {
+    "revision": "2f85b64599b5c0ce44bd52d9f589cc56",
+    "url": "/gi-pics/static/media/BW3.2f85b645.jpg"
+  }
+]);
